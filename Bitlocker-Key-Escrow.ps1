f28c83ad559b824c10b55ce1fb7a38cb8e719994
Josh Lambert <49563871+thetalljosh@users.noreply.github.com>
2023-02-24 10:17:36 -0500

    Create Bitlocker-Key-Escrow.ps1

---
+++ Bitlocker-Key-Escrow.ps1
@@ -0,0 +1,15 @@
+try{
+$BitlockerVol = Get-BitLockerVolume -MountPoint $env:SystemDrive
+        $KPID=""
+        foreach($KP in $BitlockerVol.KeyProtector){
+            if($KP.KeyProtectorType -eq "RecoveryPassword"){
+                $KPID=$KP.KeyProtectorId
+                break;
+            }
+        }
+       $output = BackupToAAD-BitLockerKeyProtector -MountPoint "$($env:SystemDrive)" -KeyProtectorId $KPID
+return $true
+}
+catch{
+     return $false
+}
