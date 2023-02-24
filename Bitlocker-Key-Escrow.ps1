9e15d18e2e6e7c74719688645f8bda0f1e7d68c6
Josh Lambert <49563871+thetalljosh@users.noreply.github.com>
2023-02-24 10:23:56 -0500

    Update Bitlocker-Key-Escrow.ps1

--- Bitlocker-Key-Escrow.ps1
+++ Bitlocker-Key-Escrow.ps1
@@ -13,3 +13,15 @@ return $true
 catch{
      return $false
 }
+
+<#
+1. Sign-in to the Microsoft Endpoint Manager admin center portal. 
+2. Browse to Devices – Windows – PowerShell Scripts
+3. Click on Add
+4. Give a Name
+5. Select the script
+6. Set Run this script using the logged on credentials as No
+7. Set Enforce script signature check to No
+8. Set Run script in 64 bit PowerShell Host as Yes
+9. Deploy to the user\device based group.
+#>
